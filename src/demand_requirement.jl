2bd56b569373a6922a2e7f8b7ec57295d1c5a309
Jose Daniel Lara <jdlara@berkeley.edu>
2024-02-29 22:02:39 -0700

    update demand specification

--- src/demand_requirement.jl
+++ src/demand_requirement.jl
@@ -1,8 +1,8 @@
-struct DemandTechnology{T <: PSY.StaticInjection} <: Technology
+struct DemandRequirement{T <: PSY.StaticInjection}
     name::String
     available::Bool
     power_systems_type::Type{T}
-    capital_cost::Float64
+    region::Union{PSY.ACBus, PSY.AggregationTopology}
     supplemental_attributes_container::IS.SupplementalAttributesContainer
     time_series_container::IS.TimeSeriesContainer
     internal::InfrastructureSystemsInternal
--- src/demand_side.jl
+++ src/demand_side.jl
@@ -1,12 +1,8 @@
-struct StorageTechnology{T <: PSY.Storage} <: Technology
+struct DemandSideTechnology{T <: PSY.StaticInjection} <: Technology
     name::String
     available::Bool
     power_systems_type::Type{T}
     capital_cost::Float64
-    battery_chemistry::String # Implement Chemistry Type Enums in PowerSystems
-    prime_mover::PSY.Primer_mover
-    capital_cost::Float64
-    operational_cost::PSY.OperationalCost
     supplemental_attributes_container::IS.SupplementalAttributesContainer
     time_series_container::IS.TimeSeriesContainer
     internal::InfrastructureSystemsInternal
