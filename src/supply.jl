344475284bbb7817be03d644f08c913009be5fa5
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2024-03-26 08:06:57 -0600

    adding some examples

--- src/supply.jl
+++ src/supply.jl
@@ -9,5 +9,30 @@ struct SupplyTechnology{T <: PSY.Generator} <: Technology
     operational_cost::Union{Nothing, PSY.OperationalCost}
     supplemental_attributes_container::IS.SupplementalAttributesContainer
     time_series_container::IS.TimeSeriesContainer
-    internal::InfrastructureSystemsInternal
+    internal::IS.InfrastructureSystemsInternal
+end
+
+
+function SupplyTechnology(name, available, power_systems_type, fuel, prime_mover, capacity_factor, operational_cost, supplemental_attributes_container, time_series_container=IS.TimeSeriesContainer(), )
+    SupplyTechnology(nunamember, available, power_systems_type, fuel, prime_mover, capacity_factor, operational_cost, supplemental_attributes_container, time_series_container, IS.InfrastructureSystemsInternal(), )
+end
+
+function SupplyTechnology(; name, available, power_systems_type, fuel, prime_mover, capacity_factor, operational_cost, supplemental_attributes_container, time_series_container=IS.TimeSeriesContainer(), internal=IS.InfrastructureSystemsInternal(), )
+    SupplyTechnology(name, available, power_systems_type, fuel, prime_mover, capacity_factor, operational_cost, supplemental_attributes_container, time_series_container, internal)
+end
+
+# Constructor for demo purposes; non-functional.
+function SupplyTechnology(::Nothing)
+    SupplyTechnology(;
+        name="init",
+        available=false,
+        power_systems_type = PSY.ThermalStandard,
+        fuel = PSY.ThermalFuels.NG,
+        prime_mover = PSY.PrimeMovers.CT,
+        capacity_factor = 0.9,
+        capital_cost = IS.FunctionData(nothing),
+        operational_cost = PSY.ThreePartCost(nothing),
+        supplemental_attributes_container = IS.SupplementalAttributesContainer(nothing),
+        time_series_container=IS.TimeSeriesContainer(),
+    )
 end
