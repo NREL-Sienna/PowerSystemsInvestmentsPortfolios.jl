825c74c18030263c56a32306043c06ec098f412b
rodrigomha <rodrigomha@gmail.com>
2024-04-03 17:25:31 -0700

    add setter methods

--- src/technologies.jl
+++ src/technologies.jl
@@ -2,12 +2,13 @@
 abstract type to represent technolgies available for investment.
 
 Required fields for a technology Type
-- name
-- available
-- power_systems_type
-- time_series_container
-- supplemental_attributes_container
-- internal
+
+  - name
+  - available
+  - power_systems_type
+  - time_series_container
+  - supplemental_attributes_container
+  - internal
 """
 abstract type Technology <: IS.InfrastructureSystemsComponent end
 
@@ -17,4 +18,13 @@ get_power_systems_type(val::Technology) = val.power_systems_type
 get_internal(val::Technology) = val.internal
 get_ext(val::Technology) = get_ext(get_internal(val))
 get_time_series_container(val::Technology) = val.time_series_container
-get_supplemental_attributes_container(val::Technology) = val.supplemental_attributes_container
+get_supplemental_attributes_container(val::Technology) =
+    val.supplemental_attributes_container
+
+set_name!(val::Technology, v::AbstractString) = val.name = v
+set_available!(val::Technology, v::Bool) = val.available = v
+# set_power_systems_type!(...)
+set_internal!(val::Technology, v::IS.InfrastructureSystemsInternal) = val.internal = v
+set_ext!(val::Technology, v::Dict{String, Any}) = val.ext = v
+# set_time_series_container!(...)
+# set_supplemental_attributes_container(...)
