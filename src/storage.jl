6b3e7db8d1d6682a4c68da4c4b924196494b698d
Pradyumna Rao <pradyumna.rao@nrel.gov>
2024-04-11 11:49:05 -0600

    changing where timeseriesdata functions are and adding tests

--- src/storage.jl
+++ src/storage.jl
@@ -12,3 +12,28 @@ struct StorageTechnology{T <: PSY.Storage} <: Technology
 end
 
 get_name(technology::StorageTechnology) = technology.name
+
+"""
+Clearing the time series for a storage device
+"""
+function clear_time_series_storage!(component::StorageTechnology)
+    return IS.clear_time_series_storage!(component)
+end
+
+
+"""
+Setting the time series for a storage device
+"""
+function set_time_series_storage!(
+    component::StorageTechnology,
+    storage::Union{Nothing, TimeSeriesStorage},
+)
+    return IS.set_time_series_storage!(component, storage)
+end
+
+"""
+Getting the time series for a storage device
+"""
+function _get_time_series_storage(component::StorageTechnology)
+    return IS._get_time_series_storage(component)
+end
--- src/technologies.jl
+++ src/technologies.jl
@@ -28,3 +28,110 @@ set_internal!(val::Technology, v::IS.InfrastructureSystemsInternal) = val.intern
 set_ext!(val::Technology, v::Dict{String, Any}) = val.ext = v
 # set_time_series_container!(...)
 # set_supplemental_attributes_container(...)
+
+"""
+Getting a time series by key
+"""
+function get_time_series_by_key(
+    key::TimeSeriesKey,
+    component::Technology;
+    start_time::Union{Nothing, Dates.DateTime} = nothing,
+    len::Union{Nothing, Int} = nothing,
+    count::Union{Nothing, Int} = nothing,
+)
+    return IS.get_time_series_by_key(
+        key,
+        component,
+        start_time=start_time,
+        len=len,
+        count=count,
+    )
+end
+
+"""
+Returns an iterator of TimeSeriesData instances attached to the component.
+"""
+function get_time_series_multiple(
+    component::Technology,
+    filter_func = nothing;
+    type = nothing,
+    start_time = nothing,
+    name = nothing,
+)
+    return IS.get_time_series_multiple(
+        component,
+        filter_func;
+        type=type,
+        start_time=start_time,
+        name=name,
+    )
+end
+
+"""
+Returns an iterator of TimeSeriesData instances attached to the component.
+"""
+function get_time_series_multiple(
+    ::Type{TimeSeriesMetadata},
+    component::Technology,
+)
+    return IS.get_time_series_multiple(component, TimeSeriesMetadata)
+end
+
+"""
+Returns the TimeSeriesData instance with the metadata that's attached to the component.
+"""
+function get_time_series_with_metadata_multiple(
+    component::Technology,
+    filter_func = nothing;
+    type = nothing,
+    start_time = nothing,
+    name = nothing,
+)
+    return IS.get_time_series_with_metadata_multiple(
+        component,
+        filter_func;
+        type=type,
+        start_time=start_time,
+        name=name,
+    )
+end
+
+"""
+Transform all instances of SingleTimeSeries to DeterministicSingleTimeSeries. Do nothing
+if the component does not contain any instances.
+
+All required checks must have been completed by the caller.
+
+Return true if a transformation occurs.
+"""
+function transform_single_time_series_internal!(
+    component::Technology,
+    ::Type{T},
+    params::TimeSeriesParameters,
+) where {T <: DeterministicSingleTimeSeries}
+    return IS.transform_single_time_series_internal!(component, T, params)
+end
+
+"""
+Method to get the time series transformed parameters
+"""
+function get_single_time_series_transformed_parameters(
+    component::Technology,
+    ::Type{T},
+    horizon::Int,
+    interval::Dates.Period,
+) where {T <: Forecast}
+    return IS.get_single_time_series_transformed_parameters(component, T, horizon, interval)
+end
+
+"""
+Get method for transformed parameters from metadata instead
+"""
+function _get_single_time_series_transformed_parameters(
+    ts_metadata::SingleTimeSeriesMetadata,
+    ::Type{T},
+    horizon::Int,
+    interval::Dates.Period,
+) where {T <: Forecast}
+    return IS._get_single_time_series_transformed_parameters(ts_metadata, T, horizon, interval)
+end
