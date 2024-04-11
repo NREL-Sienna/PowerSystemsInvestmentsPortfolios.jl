d22568c3e1a1e6399365d891a4c11f22e55bf91c
rodrigomha <rodrigomha@gmail.com>
2024-04-11 13:18:33 -0700

    remove subportfolios

--- src/portfolio.jl
+++ src/portfolio.jl
@@ -223,26 +223,16 @@ generators = collect(Portfolio.get_technologies(Generator, portfolio))
 
 See also: [`iterate_technologies`](@ref)
 """
-function get_technologies(
-    ::Type{T},
-    portfolio::Portfolio;
-    subportfoliotem_name=nothing,
-) where {T <: Technology}
-    return IS.get_components(T, portfolio.data; subportfoliotem_name=subportfoliotem_name)
+function get_technologies(::Type{T}, portfolio::Portfolio;) where {T <: Technology}
+    return IS.get_components(T, portfolio.data, nothing)
 end
 
 function get_technologies(
     filter_func::Function,
     ::Type{T},
-    portfolio::Portfolio;
-    subportfoliotem_name=nothing,
+    portfolio::Portfolio,
 ) where {T <: Technology}
-    return IS.get_components(
-        filter_func,
-        T,
-        portfolio.data;
-        subportfoliotem_name=subportfoliotem_name,
-    )
+    return IS.get_components(T, portfolio.data, filter_func)
 end
 
 # These are helper functions for debugging problems.
