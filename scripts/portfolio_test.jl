f3c391e6fa526f61d3c6a2a16b74cefabdc0a9f5
rodrigomha <rodrigomha@gmail.com>
2024-05-29 15:43:44 -0700

    add ext stuff

--- scripts/portfolio_test.jl
+++ scripts/portfolio_test.jl
@@ -11,14 +11,14 @@ const PSIP = PowerSystemsInvestmentsPortfolios
 
 data = PSY._create_system_data_from_kwargs()
 
-bus = ACBus(nothing)
-discount_rate = 0.07
-investment_schedule = Dict()
-port_metadata = PSIP.PortfolioMetadata("portfolio_test", nothing, nothing)
+#bus = ACBus(nothing)
+#discount_rate = 0.07
+#investment_schedule = Dict()
+#port_metadata = PSIP.PortfolioMetadata("portfolio_test", nothing, nothing)
 
 p = Portfolio(0.07)
 
-t = SupplyTechnology{ThermalStandard}(
+t_th = SupplyTechnology{ThermalStandard}(
     name="thermal_tech",
     available=true,
     fuel=PSY.ThermalFuels.COAL,
@@ -28,7 +28,57 @@ t = SupplyTechnology{ThermalStandard}(
     operational_cost=nothing,
 )
 
-PSIP.add_technology!(p, t)
+t_th_ext = t_th.ext
+t_th_ext["capital_cost"] = 50.0
+t_th_ext["investment_periods"] = [2030, 2040]
+# one option is to repeat these for each investment
+t_th_ext["operational_periods"] = [1, 2, 3]
+t_th_ext["operational_periods_2"] = [
+    1, 2, 3, # first investment period
+    4, 5, 6  # second investment period
+]
+t_th_ext["investment_operational_periods_map"] = Dict(
+    2030 => [1, 2, 3],
+    2040 => [4, 5, 6],
+)
+t_th_ext["initial_capacity"] = 200.0 # Data in MW
+t_th_ext["maximum_capacity"] = 10000.0
+t_th_ext["minimum_required_capacity"] = [0.0, 0.0]
+
+t_th_exp = SupplyTechnology{ThermalStandard}(
+    name="thermal_tech_expensive",
+    available=true,
+    fuel=PSY.ThermalFuels.WASTE_OIL,
+    prime_mover=PSY.PrimeMovers.ST,
+    capacity_factor=0.92, # cap factor
+    capital_cost=nothing,
+    operational_cost=nothing,
+)
+
+t_th_exp_ext = t_th_exp.ext
+t_th_exp_ext["capital_cost"] = 150.0
+
+# Initial Capacity
+
+t_re = SupplyTechnology{RenewableDispatch}(
+    name="renewable_tech",
+    available=true,
+    fuel=PSY.ThermalFuels.OTHER,
+    prime_mover=PSY.PrimeMovers.WT,
+    capacity_factor=0.98, # cap factor
+    capital_cost=nothing,
+    operational_cost=nothing,
+)
+
+t_re_ext = t_re.ext
+t_re_ext["capital_cost"] = 100.0
+t_re_ext["variable_capacity_factor"] = [0.8, 0.6, 0.3]
+
+
+PSIP.add_technology!(p, t_th)
+PSIP.add_technology!(p, t_re)
+
+
 IS.serialize(t)
 IS.serialize(p)
 
