d0a6ce3ddc855e5fa55da3e986f0568828815727
Jerry Potts <jerry.potts@utexas.edu>
2024-06-10 15:11:25 -0600

    added fields for supplytechnology

--- src/models/generated/DemandRequirement.jl
+++ src/models/generated/DemandRequirement.jl
@@ -6,39 +6,60 @@ This file is auto-generated. Do not edit.
 
 """
     mutable struct DemandRequirement{T <: PSY.StaticInjection} <: InfrastructureSystemsComponent
+        load_growth::Float
         name::String
         power_systems_type::String
+        region::String
         available::Boolean
+        peak_load::Float
     end
 
 
 
 # Arguments
+- `load_growth::Float`:
 - `name::String`:
 - `power_systems_type::String`:
+- `region::String`:
 - `available::Boolean`:
+- `peak_load::Float`:
 """
 mutable struct DemandRequirement{T <: PSY.StaticInjection} <: InfrastructureSystemsComponent
+    load_growth::Float
     name::String
     power_systems_type::String
+    region::String
     available::Boolean
+    peak_load::Float
 end
 
 
-function DemandRequirement{T}(; name, power_systems_type, available, ) where T <: PSY.StaticInjection
-    DemandRequirement{T}(name, power_systems_type, available, )
+function DemandRequirement{T}(; load_growth, name, power_systems_type, region, available, peak_load, ) where T <: PSY.StaticInjection
+    DemandRequirement{T}(load_growth, name, power_systems_type, region, available, peak_load, )
 end
 
+"""Get [`DemandRequirement`](@ref) `load_growth`."""
+get_load_growth(value::DemandRequirement) = value.load_growth
 """Get [`DemandRequirement`](@ref) `name`."""
 get_name(value::DemandRequirement) = value.name
 """Get [`DemandRequirement`](@ref) `power_systems_type`."""
 get_power_systems_type(value::DemandRequirement) = value.power_systems_type
+"""Get [`DemandRequirement`](@ref) `region`."""
+get_region(value::DemandRequirement) = value.region
 """Get [`DemandRequirement`](@ref) `available`."""
 get_available(value::DemandRequirement) = value.available
+"""Get [`DemandRequirement`](@ref) `peak_load`."""
+get_peak_load(value::DemandRequirement) = value.peak_load
 
+"""Set [`DemandRequirement`](@ref) `load_growth`."""
+set_load_growth!(value::DemandRequirement, val) = value.load_growth = val
 """Set [`DemandRequirement`](@ref) `name`."""
 set_name!(value::DemandRequirement, val) = value.name = val
 """Set [`DemandRequirement`](@ref) `power_systems_type`."""
 set_power_systems_type!(value::DemandRequirement, val) = value.power_systems_type = val
+"""Set [`DemandRequirement`](@ref) `region`."""
+set_region!(value::DemandRequirement, val) = value.region = val
 """Set [`DemandRequirement`](@ref) `available`."""
 set_available!(value::DemandRequirement, val) = value.available = val
+"""Set [`DemandRequirement`](@ref) `peak_load`."""
+set_peak_load!(value::DemandRequirement, val) = value.peak_load = val
--- src/models/generated/SupplyTechnology.jl
+++ src/models/generated/SupplyTechnology.jl
@@ -6,14 +6,18 @@ This file is auto-generated. Do not edit.
 
 """
     mutable struct SupplyTechnology{T <: PSY.Generator} <: InfrastructureSystemsComponent
+        base_power::Float
         capital_cost::Float
         minimum_required_capacity::Float
+        prime_mover_type::String
         available::Boolean
+        gen_ID::String
         name::String
         initial_capacity::Float
         fuel::String
         power_systems_type::String
         variable_cost::Float
+        balancing_topology::String
         operations_cost::Float
         maximum_capacity::Float
         capacity_factor::String
@@ -22,43 +26,57 @@ This file is auto-generated. Do not edit.
 
 
 # Arguments
+- `base_power::Float`:
 - `capital_cost::Float`:
 - `minimum_required_capacity::Float`:
+- `prime_mover_type::String`:
 - `available::Boolean`:
+- `gen_ID::String`:
 - `name::String`:
 - `initial_capacity::Float`:
 - `fuel::String`:
 - `power_systems_type::String`:
 - `variable_cost::Float`:
+- `balancing_topology::String`:
 - `operations_cost::Float`:
 - `maximum_capacity::Float`:
 - `capacity_factor::String`:
 """
 mutable struct SupplyTechnology{T <: PSY.Generator} <: InfrastructureSystemsComponent
+    base_power::Float
     capital_cost::Float
     minimum_required_capacity::Float
+    prime_mover_type::String
     available::Boolean
+    gen_ID::String
     name::String
     initial_capacity::Float
     fuel::String
     power_systems_type::String
     variable_cost::Float
+    balancing_topology::String
     operations_cost::Float
     maximum_capacity::Float
     capacity_factor::String
 end
 
 
-function SupplyTechnology{T}(; capital_cost, minimum_required_capacity, available, name, initial_capacity, fuel, power_systems_type, variable_cost, operations_cost, maximum_capacity, capacity_factor, ) where T <: PSY.Generator
-    SupplyTechnology{T}(capital_cost, minimum_required_capacity, available, name, initial_capacity, fuel, power_systems_type, variable_cost, operations_cost, maximum_capacity, capacity_factor, )
+function SupplyTechnology{T}(; base_power, capital_cost, minimum_required_capacity, prime_mover_type, available, gen_ID, name, initial_capacity, fuel, power_systems_type, variable_cost, balancing_topology, operations_cost, maximum_capacity, capacity_factor, ) where T <: PSY.Generator
+    SupplyTechnology{T}(base_power, capital_cost, minimum_required_capacity, prime_mover_type, available, gen_ID, name, initial_capacity, fuel, power_systems_type, variable_cost, balancing_topology, operations_cost, maximum_capacity, capacity_factor, )
 end
 
+"""Get [`SupplyTechnology`](@ref) `base_power`."""
+get_base_power(value::SupplyTechnology) = value.base_power
 """Get [`SupplyTechnology`](@ref) `capital_cost`."""
 get_capital_cost(value::SupplyTechnology) = value.capital_cost
 """Get [`SupplyTechnology`](@ref) `minimum_required_capacity`."""
 get_minimum_required_capacity(value::SupplyTechnology) = value.minimum_required_capacity
+"""Get [`SupplyTechnology`](@ref) `prime_mover_type`."""
+get_prime_mover_type(value::SupplyTechnology) = value.prime_mover_type
 """Get [`SupplyTechnology`](@ref) `available`."""
 get_available(value::SupplyTechnology) = value.available
+"""Get [`SupplyTechnology`](@ref) `gen_ID`."""
+get_gen_ID(value::SupplyTechnology) = value.gen_ID
 """Get [`SupplyTechnology`](@ref) `name`."""
 get_name(value::SupplyTechnology) = value.name
 """Get [`SupplyTechnology`](@ref) `initial_capacity`."""
@@ -69,6 +87,8 @@ get_fuel(value::SupplyTechnology) = value.fuel
 get_power_systems_type(value::SupplyTechnology) = value.power_systems_type
 """Get [`SupplyTechnology`](@ref) `variable_cost`."""
 get_variable_cost(value::SupplyTechnology) = value.variable_cost
+"""Get [`SupplyTechnology`](@ref) `balancing_topology`."""
+get_balancing_topology(value::SupplyTechnology) = value.balancing_topology
 """Get [`SupplyTechnology`](@ref) `operations_cost`."""
 get_operations_cost(value::SupplyTechnology) = value.operations_cost
 """Get [`SupplyTechnology`](@ref) `maximum_capacity`."""
@@ -76,12 +96,18 @@ get_maximum_capacity(value::SupplyTechnology) = value.maximum_capacity
 """Get [`SupplyTechnology`](@ref) `capacity_factor`."""
 get_capacity_factor(value::SupplyTechnology) = value.capacity_factor
 
+"""Set [`SupplyTechnology`](@ref) `base_power`."""
+set_base_power!(value::SupplyTechnology, val) = value.base_power = val
 """Set [`SupplyTechnology`](@ref) `capital_cost`."""
 set_capital_cost!(value::SupplyTechnology, val) = value.capital_cost = val
 """Set [`SupplyTechnology`](@ref) `minimum_required_capacity`."""
 set_minimum_required_capacity!(value::SupplyTechnology, val) = value.minimum_required_capacity = val
+"""Set [`SupplyTechnology`](@ref) `prime_mover_type`."""
+set_prime_mover_type!(value::SupplyTechnology, val) = value.prime_mover_type = val
 """Set [`SupplyTechnology`](@ref) `available`."""
 set_available!(value::SupplyTechnology, val) = value.available = val
+"""Set [`SupplyTechnology`](@ref) `gen_ID`."""
+set_gen_ID!(value::SupplyTechnology, val) = value.gen_ID = val
 """Set [`SupplyTechnology`](@ref) `name`."""
 set_name!(value::SupplyTechnology, val) = value.name = val
 """Set [`SupplyTechnology`](@ref) `initial_capacity`."""
@@ -92,6 +118,8 @@ set_fuel!(value::SupplyTechnology, val) = value.fuel = val
 set_power_systems_type!(value::SupplyTechnology, val) = value.power_systems_type = val
 """Set [`SupplyTechnology`](@ref) `variable_cost`."""
 set_variable_cost!(value::SupplyTechnology, val) = value.variable_cost = val
+"""Set [`SupplyTechnology`](@ref) `balancing_topology`."""
+set_balancing_topology!(value::SupplyTechnology, val) = value.balancing_topology = val
 """Set [`SupplyTechnology`](@ref) `operations_cost`."""
 set_operations_cost!(value::SupplyTechnology, val) = value.operations_cost = val
 """Set [`SupplyTechnology`](@ref) `maximum_capacity`."""
