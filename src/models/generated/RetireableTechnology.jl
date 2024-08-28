f43645dacf85fe34ab858c4648e1279dc507b1ca
Jerry Potts <jerry.potts@utexas.edu>
2024-08-28 10:19:54 -0600

    change retrofits and retirements to supplemental attributes

--- src/models/generated/RetireableTechnology.jl
+++ src/models/generated/RetireableTechnology.jl
@@ -5,59 +5,43 @@ This file is auto-generated. Do not edit.
 #! format: off
 
 """
-    mutable struct RetireableTechnology{T <: PSY.Generator} <: Technology
+    mutable struct RetireableTechnology <: IS.SupplementalAttribute
         name::String
-        can_retire::Dict{PrimeMovers,Dict{String, Int64}}
-        power_systems_type::String
         internal::InfrastructureSystemsInternal
-        ext::Dict
+        retireable_capacity::Dict{String, Float64}
     end
 
 
 
 # Arguments
 - `name::String`: The technology name
-- `can_retire::Dict{PrimeMovers,Dict{String, Int64}}`: (default: `Dict()`) Dictionary of PrimeMovers in each region that can retire
-- `power_systems_type::String`: maps to a valid PowerSystems.jl for PCM modeling
 - `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) Internal field
-- `ext::Dict`: (default: `Dict()`) Option for providing additional data
+- `retireable_capacity::Dict{String, Float64}`: (default: `0.0`) Amount of existing capacity for technology that can retire
 """
-mutable struct RetireableTechnology{T <: PSY.Generator} <: Technology
+mutable struct RetireableTechnology <: IS.SupplementalAttribute
     "The technology name"
     name::String
-    "Dictionary of PrimeMovers in each region that can retire"
-    can_retire::Dict{PrimeMovers,Dict{String, Int64}}
-    "maps to a valid PowerSystems.jl for PCM modeling"
-    power_systems_type::String
     "Internal field"
     internal::InfrastructureSystemsInternal
-    "Option for providing additional data"
-    ext::Dict
+    "Amount of existing capacity for technology that can retire"
+    retireable_capacity::Dict{String, Float64}
 end
 
 
-function RetireableTechnology{T}(; name, can_retire=Dict(), power_systems_type, internal=InfrastructureSystemsInternal(), ext=Dict(), ) where T <: PSY.Generator
-    RetireableTechnology{T}(name, can_retire, power_systems_type, internal, ext, )
+function RetireableTechnology(; name, internal=InfrastructureSystemsInternal(), retireable_capacity=0.0, )
+    RetireableTechnology(name, internal, retireable_capacity, )
 end
 
 """Get [`RetireableTechnology`](@ref) `name`."""
 get_name(value::RetireableTechnology) = value.name
-"""Get [`RetireableTechnology`](@ref) `can_retire`."""
-get_can_retire(value::RetireableTechnology) = value.can_retire
-"""Get [`RetireableTechnology`](@ref) `power_systems_type`."""
-get_power_systems_type(value::RetireableTechnology) = value.power_systems_type
 """Get [`RetireableTechnology`](@ref) `internal`."""
 get_internal(value::RetireableTechnology) = value.internal
-"""Get [`RetireableTechnology`](@ref) `ext`."""
-get_ext(value::RetireableTechnology) = value.ext
+"""Get [`RetireableTechnology`](@ref) `retireable_capacity`."""
+get_retireable_capacity(value::RetireableTechnology) = value.retireable_capacity
 
 """Set [`RetireableTechnology`](@ref) `name`."""
 set_name!(value::RetireableTechnology, val) = value.name = val
-"""Set [`RetireableTechnology`](@ref) `can_retire`."""
-set_can_retire!(value::RetireableTechnology, val) = value.can_retire = val
-"""Set [`RetireableTechnology`](@ref) `power_systems_type`."""
-set_power_systems_type!(value::RetireableTechnology, val) = value.power_systems_type = val
 """Set [`RetireableTechnology`](@ref) `internal`."""
 set_internal!(value::RetireableTechnology, val) = value.internal = val
-"""Set [`RetireableTechnology`](@ref) `ext`."""
-set_ext!(value::RetireableTechnology, val) = value.ext = val
+"""Set [`RetireableTechnology`](@ref) `retireable_capacity`."""
+set_retireable_capacity!(value::RetireableTechnology, val) = value.retireable_capacity = val
--- src/models/generated/RetrofitTechnology.jl
+++ src/models/generated/RetrofitTechnology.jl
@@ -5,75 +5,67 @@ This file is auto-generated. Do not edit.
 #! format: off
 
 """
-    mutable struct RetrofitTechnology{T <: PSY.Generator} <: Technology
+    mutable struct RetrofitTechnology <: IS.SupplementalAttribute
         name::String
-        retrofit_id::Dict{PrimeMovers,Dict{String, Int64}}
-        power_systems_type::String
+        retrofit_id::Int64
         retrofit_efficiency::Dict{PrimeMovers,Dict{String, Float64}}
         internal::InfrastructureSystemsInternal
         ext::Dict
-        can_retrofit::Dict{PrimeMovers,Dict{String, Int64}}
+        retrofit_capacity::Float64
     end
 
 
 
 # Arguments
 - `name::String`: The technology name
-- `retrofit_id::Dict{PrimeMovers,Dict{String, Int64}}`: (default: `Dict()`) Dictionary of unique identifiers to group retrofittable source technologies with retrofit options inside the same zone.
-- `power_systems_type::String`: maps to a valid PowerSystems.jl for PCM modeling
+- `retrofit_id::Int64`: (default: `0`) Unique identifier to group retrofittable source technologies with retrofit options inside the same zone.
 - `retrofit_efficiency::Dict{PrimeMovers,Dict{String, Float64}}`: (default: `Dict()`) Dictionary of Efficiency of the retrofit technology.
 - `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) Internal field
 - `ext::Dict`: (default: `Dict()`) Option for providing additional data
-- `can_retrofit::Dict{PrimeMovers,Dict{String, Int64}}`: (default: `Dict()`) Dictionary of PrimeMovers in each region that can retire
+- `retrofit_capacity::Float64`: (default: `0.0`) Amount of existing capacity for technology that can be retrofitted
 """
-mutable struct RetrofitTechnology{T <: PSY.Generator} <: Technology
+mutable struct RetrofitTechnology <: IS.SupplementalAttribute
     "The technology name"
     name::String
-    "Dictionary of unique identifiers to group retrofittable source technologies with retrofit options inside the same zone."
-    retrofit_id::Dict{PrimeMovers,Dict{String, Int64}}
-    "maps to a valid PowerSystems.jl for PCM modeling"
-    power_systems_type::String
+    "Unique identifier to group retrofittable source technologies with retrofit options inside the same zone."
+    retrofit_id::Int64
     "Dictionary of Efficiency of the retrofit technology."
     retrofit_efficiency::Dict{PrimeMovers,Dict{String, Float64}}
     "Internal field"
     internal::InfrastructureSystemsInternal
     "Option for providing additional data"
     ext::Dict
-    "Dictionary of PrimeMovers in each region that can retire"
-    can_retrofit::Dict{PrimeMovers,Dict{String, Int64}}
+    "Amount of existing capacity for technology that can be retrofitted"
+    retrofit_capacity::Float64
 end
 
 
-function RetrofitTechnology{T}(; name, retrofit_id=Dict(), power_systems_type, retrofit_efficiency=Dict(), internal=InfrastructureSystemsInternal(), ext=Dict(), can_retrofit=Dict(), ) where T <: PSY.Generator
-    RetrofitTechnology{T}(name, retrofit_id, power_systems_type, retrofit_efficiency, internal, ext, can_retrofit, )
+function RetrofitTechnology(; name, retrofit_id=0, retrofit_efficiency=Dict(), internal=InfrastructureSystemsInternal(), ext=Dict(), retrofit_capacity=0.0, )
+    RetrofitTechnology(name, retrofit_id, retrofit_efficiency, internal, ext, retrofit_capacity, )
 end
 
 """Get [`RetrofitTechnology`](@ref) `name`."""
 get_name(value::RetrofitTechnology) = value.name
 """Get [`RetrofitTechnology`](@ref) `retrofit_id`."""
 get_retrofit_id(value::RetrofitTechnology) = value.retrofit_id
-"""Get [`RetrofitTechnology`](@ref) `power_systems_type`."""
-get_power_systems_type(value::RetrofitTechnology) = value.power_systems_type
 """Get [`RetrofitTechnology`](@ref) `retrofit_efficiency`."""
 get_retrofit_efficiency(value::RetrofitTechnology) = value.retrofit_efficiency
 """Get [`RetrofitTechnology`](@ref) `internal`."""
 get_internal(value::RetrofitTechnology) = value.internal
 """Get [`RetrofitTechnology`](@ref) `ext`."""
 get_ext(value::RetrofitTechnology) = value.ext
-"""Get [`RetrofitTechnology`](@ref) `can_retrofit`."""
-get_can_retrofit(value::RetrofitTechnology) = value.can_retrofit
+"""Get [`RetrofitTechnology`](@ref) `retrofit_capacity`."""
+get_retrofit_capacity(value::RetrofitTechnology) = value.retrofit_capacity
 
 """Set [`RetrofitTechnology`](@ref) `name`."""
 set_name!(value::RetrofitTechnology, val) = value.name = val
 """Set [`RetrofitTechnology`](@ref) `retrofit_id`."""
 set_retrofit_id!(value::RetrofitTechnology, val) = value.retrofit_id = val
-"""Set [`RetrofitTechnology`](@ref) `power_systems_type`."""
-set_power_systems_type!(value::RetrofitTechnology, val) = value.power_systems_type = val
 """Set [`RetrofitTechnology`](@ref) `retrofit_efficiency`."""
 set_retrofit_efficiency!(value::RetrofitTechnology, val) = value.retrofit_efficiency = val
 """Set [`RetrofitTechnology`](@ref) `internal`."""
 set_internal!(value::RetrofitTechnology, val) = value.internal = val
 """Set [`RetrofitTechnology`](@ref) `ext`."""
 set_ext!(value::RetrofitTechnology, val) = value.ext = val
-"""Set [`RetrofitTechnology`](@ref) `can_retrofit`."""
-set_can_retrofit!(value::RetrofitTechnology, val) = value.can_retrofit = val
+"""Set [`RetrofitTechnology`](@ref) `retrofit_capacity`."""
+set_retrofit_capacity!(value::RetrofitTechnology, val) = value.retrofit_capacity = val
