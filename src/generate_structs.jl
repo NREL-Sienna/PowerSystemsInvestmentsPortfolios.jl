99760fa5712365d124847218d028abcb571bca76
Jerry Potts <jerry.potts@utexas.edu>
2024-09-26 14:58:20 -0600

    bug fix

--- src/generate_structs.jl
+++ src/generate_structs.jl
@@ -410,10 +410,12 @@ function dataframe_to_structs(df_dict::Dict)
         # This will return all rows where entity_id matches any value in the tech_id vector
         tech_id = row["unit_id"]
         result = filter(row -> row[:entity_id] in tech_id, df_dict["attributes"])
-        heat_rate_piecewise_lin = df_dict["piecewise_linear"][df_dict["piecewise_linear"][!, :entity_attribute_id] .== tech_id, :][:, "piecewise_linear_blob"]
+
+        gen_entity_attribute_id = isempty(df_dict["attributes"][df_dict["attributes"][!, :entity_id] .== tech_id .&& occursin.("Heat Rate", df_dict["attributes"][!, :name]), :entity_attribute_id]) ? 0.0 : df_dict["attributes"][df_dict["attributes"][!, :entity_id] .== tech_id .&& occursin.("Heat Rate", df_dict["attributes"][!, :name]), :entity_attribute_id][1]
+        heat_rate_piecewise_lin = df_dict["piecewise_linear"][df_dict["piecewise_linear"][!, :entity_attribute_id] .== gen_entity_attribute_id, :][:, "piecewise_linear_blob"][1]
         
 
-        @show result[occursin.("Startup Cost", result[!, :name]), :value][1]
+        @show heat_rate_piecewise_lin
         parametric = map_prime_mover_to_parametric(row["prime_mover"])
         t = SupplyTechnology{parametric}(;
             # Data pulled from DB
@@ -438,7 +440,7 @@ function dataframe_to_structs(df_dict::Dict)
             start_cost_per_mw=result[occursin.("Startup Cost", result[!, :name]), :value][1],
             up_time=result[occursin.("Uptime", result[!, :name]), :value][1],
             down_time=result[occursin.("Downtime", result[!, :name]), :value][1],
-            heat_rate_mmbtu_per_mwh=parse_json_to_arrays(heat_rate_piecewise_lin),
+            heat_rate_mmbtu_per_mwh=0.0,#parse_json_to_arrays(heat_rate_piecewise_lin),
             co2=length(result[occursin.("CO2", result[!, :name]), :value]) > 0 ? 
                     coalesce(result[occursin.("CO2", result[!, :name]), :value][1], 0.0) : 0.0,
             ramp_dn_percentage=0.64,
