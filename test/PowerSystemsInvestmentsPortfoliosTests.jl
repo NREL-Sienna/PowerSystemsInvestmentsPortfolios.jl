acda0b0c236aa9b3991c9d0083cda6d0b18d9fc6
Jose Daniel Lara <jdlara@berkeley.edu>
2026-01-13 12:36:28 -0700

    fix artifacts file

--- test/PowerSystemsInvestmentsPortfoliosTests.jl
+++ test/PowerSystemsInvestmentsPortfoliosTests.jl
@@ -89,46 +89,47 @@ function get_logging_level_from_env(env_name::String, default)
     return IS.get_logging_level(level)
 end
 
-function run_tests()
-    logging_config_filename = get(ENV, "Sienna_LOGGING_CONFIG", nothing)
-    if logging_config_filename !== nothing
-        config = IS.LoggingConfiguration(logging_config_filename)
-    else
-        config = IS.LoggingConfiguration(;
-            filename=LOG_FILE,
-            file_level=Logging.Info,
-            console_level=Logging.Error,
-        )
-    end
-    console_logger = ConsoleLogger(config.console_stream, config.console_level)
-
-    IS.open_file_logger(config.filename, config.file_level) do file_logger
-        levels = (Logging.Info, Logging.Warn, Logging.Error)
-        multi_logger =
-            IS.MultiLogger([console_logger, file_logger], IS.LogEventTracker(levels))
-        global_logger(multi_logger)
-
-        if !isempty(config.group_levels)
-            IS.set_group_levels!(multi_logger, config.group_levels)
+function run_tests(args...; kwargs...)
+    logger = global_logger()
+    try
+        logging_config_filename = get(ENV, "SIENNA_LOGGING_CONFIG", nothing)
+        if logging_config_filename !== nothing
+            config = IS.LoggingConfiguration(logging_config_filename)
+        else
+            config = IS.LoggingConfiguration(;
+                filename = LOG_FILE,
+                file_level = get_logging_level_from_env("SIENNA_FILE_LOG_LEVEL", "Info"),
+                console_level = get_logging_level_from_env(
+                    "SIENNA_CONSOLE_LOG_LEVEL",
+                    "Error",
+                ),
+            )
         end
+        console_logger = TerminalLogger(config.console_stream, config.console_level)
 
-        # Testing Topological components of the schema
-        @time @testset "Begin PowerSystemsInvestmentsPortfolios tests" begin
-            @includetests ARGS
+        IS.open_file_logger(config.filename, config.file_level) do file_logger
+            levels = (Logging.Info, Logging.Warn, Logging.Error)
+            multi_logger =
+                IS.MultiLogger([console_logger, file_logger], IS.LogEventTracker(levels))
+            global_logger(multi_logger)
+
+            if !isempty(config.group_levels)
+                IS.set_group_levels!(multi_logger, config.group_levels)
+            end
+
+            @time retest(args...; kwargs...)
+            @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 0
+            @info IS.report_log_summary(multi_logger)
         end
-
-        @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 0
-        @info IS.report_log_summary(multi_logger)
+    finally
+        # Guarantee that the global logger is reset.
+        global_logger(logger)
+        nothing
     end
 end
 
-logger = global_logger()
+export run_tests
 
-try
-    run_tests()
-finally
-    # Guarantee that the global logger is reset.
-    global_logger(logger)
-    nothing
 end
-end
+
+using .InfrastructureSystemsTests
