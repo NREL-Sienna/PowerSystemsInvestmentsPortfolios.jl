64d072ffacd4a83f21f40d773844d0c062064f1f
Jerry Potts <37674411+jerrypotts@users.noreply.github.com>
2026-07-02 14:15:45 -0600

    add TopologyMapping to list of DB types

--- src/db_interface/translation_constants.jl
+++ src/db_interface/translation_constants.jl
@@ -234,6 +234,7 @@ const ALL_SA_PSIP_TYPES = [
     RetirementPotential,
     AggregateRetrofitPotential,
     RetrofitPotential,
+    TopologyMapping
 ]
 
 const ALL_SA_PSIP_OPENAPI_TYPES = [
@@ -242,6 +243,7 @@ const ALL_SA_PSIP_OPENAPI_TYPES = [
     PowerOpenAPIModels.RetirementPotential,
     PowerOpenAPIModels.AggregateRetrofitPotential,
     PowerOpenAPIModels.RetrofitPotential,
+    PowerOpenAPIModels.TopologyMapping
 ]
 
 const SA_PSIP_TO_OPENAPI =
