abddb848a7ef0d8dda5e15583d9470412c1e725f
Jerry Potts <37674411+jerrypotts@users.noreply.github.com>
2026-07-23 13:45:17 -0400

    add optional cost for retirement and retrofits

--- src/models/generated/RetirementPotential.jl
+++ src/models/generated/RetirementPotential.jl
@@ -9,6 +9,7 @@ This file is auto-generated. Do not edit.
         planned_retirement_year::Dict{String, Int64}
         eligible_generators::Vector{String}
         internal::InfrastructureSystemsInternal
+        retirement_cost::PSY.ValueCurve
         ext::Dict
         build_year::Dict{String, Int64}
     end
@@ -16,28 +17,31 @@ This file is auto-generated. Do not edit.
 Supplemental attribute used to define what existing generators are eligible for retirement for a SupplyTechnology
 
 # Arguments
-- `planned_retirement_year::Dict{String, Int64}`: (default: `Dict{String, Int64}()`) Optional dictionary to indicate the year in which the forced/planned retirement will occur
+- `planned_retirement_year::Dict{String, Int64}`: (default: `Dict{String, Int64}()`) Optional dictionary to indicate the year in which the forced/planned retirement will occur. If not specified, the lifetime of the technology will be used to determine the planned retirement year.
 - `eligible_generators::Vector{String}`: (default: `Vector()`) Names of individual generation units mapped to a technology that are eligible for retirement
 - `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) (**Do not modify.**) PowerSystemsInvestmentsPortfolios.jl internal reference
+- `retirement_cost::PSY.ValueCurve`: (default: `LinearCurve(0.0)`) Cost of retiring an existing generator. (USD/MW)
 - `ext::Dict`: (default: `Dict()`) Optional dictionary to provide additional data
-- `build_year::Dict{String, Int64}`: (default: `Dict{String, Int64}()`) Optional dictionary to indicate the year in which existing generators in the base system were built
+- `build_year::Dict{String, Int64}`: (default: `Dict{String, Int64}()`) Optional dictionary to indicate the year in which existing generators in the base system were built. If not specified, it will be assumed that the generator was built in the first year of the simulation.
 """
 mutable struct RetirementPotential <: IS.SupplementalAttribute
-    "Optional dictionary to indicate the year in which the forced/planned retirement will occur"
+    "Optional dictionary to indicate the year in which the forced/planned retirement will occur. If not specified, the lifetime of the technology will be used to determine the planned retirement year."
     planned_retirement_year::Dict{String, Int64}
     "Names of individual generation units mapped to a technology that are eligible for retirement"
     eligible_generators::Vector{String}
     "(**Do not modify.**) PowerSystemsInvestmentsPortfolios.jl internal reference"
     internal::InfrastructureSystemsInternal
+    "Cost of retiring an existing generator. (USD/MW)"
+    retirement_cost::PSY.ValueCurve
     "Optional dictionary to provide additional data"
     ext::Dict
-    "Optional dictionary to indicate the year in which existing generators in the base system were built"
+    "Optional dictionary to indicate the year in which existing generators in the base system were built. If not specified, it will be assumed that the generator was built in the first year of the simulation."
     build_year::Dict{String, Int64}
 end
 
 
-function RetirementPotential(; planned_retirement_year=Dict{String, Int64}(), eligible_generators=Vector(), internal=InfrastructureSystemsInternal(), ext=Dict(), build_year=Dict{String, Int64}(), )
-    RetirementPotential(planned_retirement_year, eligible_generators, internal, ext, build_year, )
+function RetirementPotential(; planned_retirement_year=Dict{String, Int64}(), eligible_generators=Vector(), internal=InfrastructureSystemsInternal(), retirement_cost=LinearCurve(0.0), ext=Dict(), build_year=Dict{String, Int64}(), )
+    RetirementPotential(planned_retirement_year, eligible_generators, internal, retirement_cost, ext, build_year, )
 end
 
 # Constructor for demo purposes; non-functional.
@@ -46,6 +50,7 @@ function RetirementPotential(::Nothing)
         planned_retirement_year=Dict{String, Int64}(),
         eligible_generators=Dict{String, Int64}(),
         internal=Dict{String, Int64}(),
+        retirement_cost=Dict{String, Int64}(),
         ext=Dict{String, Int64}(),
         build_year=Dict{String, Int64}(),
     )
@@ -57,6 +62,8 @@ get_planned_retirement_year(value::RetirementPotential) = value.planned_retireme
 get_eligible_generators(value::RetirementPotential) = value.eligible_generators
 """Get [`RetirementPotential`](@ref) `internal`."""
 get_internal(value::RetirementPotential) = value.internal
+"""Get [`RetirementPotential`](@ref) `retirement_cost`."""
+get_retirement_cost(value::RetirementPotential) = value.retirement_cost
 """Get [`RetirementPotential`](@ref) `ext`."""
 get_ext(value::RetirementPotential) = value.ext
 """Get [`RetirementPotential`](@ref) `build_year`."""
@@ -68,6 +75,8 @@ set_planned_retirement_year!(value::RetirementPotential, val) = value.planned_re
 set_eligible_generators!(value::RetirementPotential, val) = value.eligible_generators = val
 """Set [`RetirementPotential`](@ref) `internal`."""
 set_internal!(value::RetirementPotential, val) = value.internal = val
+"""Set [`RetirementPotential`](@ref) `retirement_cost`."""
+set_retirement_cost!(value::RetirementPotential, val) = value.retirement_cost = val
 """Set [`RetirementPotential`](@ref) `ext`."""
 set_ext!(value::RetirementPotential, val) = value.ext = val
 """Set [`RetirementPotential`](@ref) `build_year`."""
--- src/models/generated/RetrofitPotential.jl
+++ src/models/generated/RetrofitPotential.jl
@@ -7,53 +7,71 @@ This file is auto-generated. Do not edit.
 """
     mutable struct RetrofitPotential <: IS.SupplementalAttribute
         eligible_generators::Vector{String}
+        retrofit_fraction::Float64
         internal::InfrastructureSystemsInternal
         ext::Dict
+        retrofit_cost::PSY.ValueCurve
     end
 
 Supplemental attribute used to define what existing generators are eligible for retrofit for a SupplyTechnology
 
 # Arguments
 - `eligible_generators::Vector{String}`: (default: `Vector()`) Names of individual generation units mapped to this technology that can be retrofitted
+- `retrofit_fraction::Float64`: (default: `1.0`) Fraction of existing capacity that is eligible for retrofits.
 - `internal::InfrastructureSystemsInternal`: (default: `InfrastructureSystemsInternal()`) (**Do not modify.**) PowerSystemsInvestmentsPortfolios.jl internal reference
 - `ext::Dict`: (default: `Dict()`) Optional dictionary to provide additional data
+- `retrofit_cost::PSY.ValueCurve`: (default: `LinearCurve(0.0)`) Cost of retrofitting an existing generator to a new technology. (USD/MW)
 """
 mutable struct RetrofitPotential <: IS.SupplementalAttribute
     "Names of individual generation units mapped to this technology that can be retrofitted"
     eligible_generators::Vector{String}
+    "Fraction of existing capacity that is eligible for retrofits."
+    retrofit_fraction::Float64
     "(**Do not modify.**) PowerSystemsInvestmentsPortfolios.jl internal reference"
     internal::InfrastructureSystemsInternal
     "Optional dictionary to provide additional data"
     ext::Dict
+    "Cost of retrofitting an existing generator to a new technology. (USD/MW)"
+    retrofit_cost::PSY.ValueCurve
 end
 
 
-function RetrofitPotential(; eligible_generators=Vector(), internal=InfrastructureSystemsInternal(), ext=Dict(), )
-    RetrofitPotential(eligible_generators, internal, ext, )
+function RetrofitPotential(; eligible_generators=Vector(), retrofit_fraction=1.0, internal=InfrastructureSystemsInternal(), ext=Dict(), retrofit_cost=LinearCurve(0.0), )
+    RetrofitPotential(eligible_generators, retrofit_fraction, internal, ext, retrofit_cost, )
 end
 
 # Constructor for demo purposes; non-functional.
 function RetrofitPotential(::Nothing)
     RetrofitPotential(;
-        eligible_generators=Dict(),
-        internal=Dict(),
-        ext=Dict(),
+        eligible_generators=LinearCurve(0.0),
+        retrofit_fraction=LinearCurve(0.0),
+        internal=LinearCurve(0.0),
+        ext=LinearCurve(0.0),
+        retrofit_cost=LinearCurve(0.0),
     )
 end
 
 """Get [`RetrofitPotential`](@ref) `eligible_generators`."""
 get_eligible_generators(value::RetrofitPotential) = value.eligible_generators
+"""Get [`RetrofitPotential`](@ref) `retrofit_fraction`."""
+get_retrofit_fraction(value::RetrofitPotential) = value.retrofit_fraction
 """Get [`RetrofitPotential`](@ref) `internal`."""
 get_internal(value::RetrofitPotential) = value.internal
 """Get [`RetrofitPotential`](@ref) `ext`."""
 get_ext(value::RetrofitPotential) = value.ext
+"""Get [`RetrofitPotential`](@ref) `retrofit_cost`."""
+get_retrofit_cost(value::RetrofitPotential) = value.retrofit_cost
 
 """Set [`RetrofitPotential`](@ref) `eligible_generators`."""
 set_eligible_generators!(value::RetrofitPotential, val) = value.eligible_generators = val
+"""Set [`RetrofitPotential`](@ref) `retrofit_fraction`."""
+set_retrofit_fraction!(value::RetrofitPotential, val) = value.retrofit_fraction = val
 """Set [`RetrofitPotential`](@ref) `internal`."""
 set_internal!(value::RetrofitPotential, val) = value.internal = val
 """Set [`RetrofitPotential`](@ref) `ext`."""
 set_ext!(value::RetrofitPotential, val) = value.ext = val
+"""Set [`RetrofitPotential`](@ref) `retrofit_cost`."""
+set_retrofit_cost!(value::RetrofitPotential, val) = value.retrofit_cost = val
 
 function serialize_openapi_struct(technology::RetrofitPotential, vals...)
     base_struct = APIServer.RetrofitPotential(; vals...)
